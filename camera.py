196b1a3b88fb41ff3fde611c0fd8702511e6fa81
Kimjunseon <junseon0722@sainti.co.kr>
2024-01-04 15:29:45 +0900

    hi

--- camera.py
+++ camera.py
@@ -11,7 +11,7 @@ camera = PiCamera()                // camera라는 객체를 만듭니다.
 
 camera.start_preview()             // 카메라 미리보기를 실행합니다.
 sleep(5)                            // 5초간 대기합니다.
-camera.capture('/home/pi/test.jpg')            //카메라를 사용해서 사진을 찍습니다.
+camera.capture('/home/admin/AIimage/image/test.jpg')  //카메라를 사용해서 사진을 찍습니다.
 camera.stop_preview()              // 카메라 미리보기를 정지합니다.
 
 # sudo apt-get install python-picamera python3-picamera
