7fc4051d19934d5adf624d3988796b9cfd3d447c
Kimjunseon <junseon0722@sainti.co.kr>
2024-01-05 14:05:43 +0900

    typo modi

--- bottle_recognition.py
+++ bottle_recognition.py
@@ -34,8 +34,7 @@ class Target:
                     'device_name': 'cpu',
                     'imagePath': 'C:/Users/khson/Desktop/new/sampleimage'
                 }
-                files = {'file': open("C:/Users/khson/Desktop/new/sampleimage", 'rb')}
-                response = requests.get(url, params=form_data, files = files)
+                response = requests.get(url, params=form_data)
                 if response.status_code == 200:
                     print('success')
                     print('respone:', response.text)
