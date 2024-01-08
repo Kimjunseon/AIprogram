f7373aac6b115c61739bb109b374221e3208da4a
Kimjunseon <junseon0722@sainti.co.kr>
2024-01-08 10:31:04 +0900

    modbus

---
+++ modbustest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+pip install pymodbus
+
+
+# In[7]:
+
+
+from pymodbus.client import ModbusTcpClient
+from pymodbus.transaction import *
+
+
+# In[8]:
+
+
+client = ModbusTcpClient('192.168.0.63', 502)
+for n in range(0, 3):
+    result = client.read_coils(n, 1)
+    print(result.bits)
+client.write_coil(0, True)
+
